4b5b0d0863ba8deeea29cf2d678477e2c1b5f267
Andrew Setiono <andrewsetiono98@gmail.com>
2026-03-23 22:53:23 +0100

    feat(yazi): modify git signs in yazi

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,2 +1,10 @@
+-- Style for git
+th.git = th.git or {}
+th.git.unknown_sign = " "
+th.git.modified_sign = "M"
+th.git.deleted_sign = "D"
+th.git.clean_sign = "✔"
+
+-- Call plugins
 require("git"):setup({ order = 1500 })
 require("duckdb"):setup()
