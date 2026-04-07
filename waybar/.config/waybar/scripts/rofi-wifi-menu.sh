24a380fdb5f80e3f33ace74fedd73514f84a16b8
Andrew Setiono <asetiono@RN-145-90-141-200.eduroam.rug.nl>
2026-04-07 11:53:58 +0200

    feat(waybar): modify rofi-wifi-menu to handle eduroam

--- waybar/.config/waybar/scripts/rofi-wifi-menu.sh
+++ waybar/.config/waybar/scripts/rofi-wifi-menu.sh
@@ -6,33 +6,45 @@ wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  
 
 connected=$(nmcli -fields WIFI g)
 if [[ "$connected" =~ "enabled" ]]; then
-	toggle="󰖪  Disable Wi-Fi"
+  toggle="󰖪  Disable Wi-Fi"
 elif [[ "$connected" =~ "disabled" ]]; then
-	toggle="󰖩  Enable Wi-Fi"
+  toggle="󰖩  Enable Wi-Fi"
 fi
 
 # Use rofi to select wifi network
-chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " )
+chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: ")
 # Get name of connection
-read -r chosen_id <<< "${chosen_network:3}"
+read -r chosen_id <<<"${chosen_network:3}"
 
 if [ "$chosen_network" = "" ]; then
-	exit
+  exit
 elif [ "$chosen_network" = "󰖩  Enable Wi-Fi" ]; then
-	nmcli radio wifi on
+  nmcli radio wifi on
 elif [ "$chosen_network" = "󰖪  Disable Wi-Fi" ]; then
-	nmcli radio wifi off
+  nmcli radio wifi off
 else
-	# Message to show when connection is activated successfully
-  	success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
-	# Get saved connections
-	saved_connections=$(nmcli -g NAME connection)
-	if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
-		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
-	else
-		if [[ "$chosen_network" =~ "" ]]; then
-			wifi_password=$(rofi -dmenu -p "Password: " )
-		fi
-		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
+  # Message to show when connection is activated successfully
+  success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
+  # Get saved connections
+  saved_connections=$(nmcli -g NAME connection)
+  if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
+    nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
+  else
+    if [[ "$chosen_id" == "eduroam" ]]; then
+      wifi_username=$(rofi -dmenu -p "Username (e.g., user@institution.domain): ")
+      wifi_password=$(rofi -dmenu -p "Password: ")
+      nmcli connection add type wifi con-name "eduroam" ifname wlan0 ssid "eduroam"
+      nmcli connection modify "eduroam" wifi-sec.key-mgmt wpa-eap
+      nmcli connection modify "eduroam" 802-1x.eap ttls
+      nmcli connection modify "eduroam" 802-1x.phase2-auth mschapv2
+      nmcli connection modify "eduroam" 802-1x.identity "$wifi_username"
+      nmcli connection modify "eduroam" 802-1x.password "$wifi_password"
+      nmcli connection up "eduroam" | grep "successfully" && notify-send "Connection Established" "$success_message"
+    else
+      if [[ "$chosen_network" =~ "" ]]; then
+        wifi_password=$(rofi -dmenu -p "Password: ")
+      fi
+      nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
     fi
+  fi
 fi
